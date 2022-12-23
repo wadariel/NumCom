8a09d3be5ab351e14b191a7ed12b3b442b90a449
darius <2189873>
2022-12-23 23:14:25 +0300

    Start presentation commit

--- app/src/main/java/com/example/numcom/presentation/MainActivity.kt
+++ app/src/main/java/com/example/numcom/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.numcom
+package com.example.numcom.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.numcom.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
