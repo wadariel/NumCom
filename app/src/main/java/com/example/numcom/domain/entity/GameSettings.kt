bb2e5caa17f4b4906583c3767be165ac7484cc45
darius <2189873>
2022-12-24 16:05:07 +0300

    Start to reload

--- app/src/main/java/com/example/numcom/domain/entity/GameSettings.kt
+++ app/src/main/java/com/example/numcom/domain/entity/GameSettings.kt
@@ -1,8 +1,12 @@
 package com.example.numcom.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class GameSettings (
     val maxSumValue: Int,
     val minCountOfRightAnswers: Int,
     val minPercentOfRightAnswers: Int,
     val gameTimeInSeconds: Int
-        ): java.io.Serializable
+        ): Parcelable
